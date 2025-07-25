ed2e04f9e8b4075f8ec9a3ca5bab1778c87dbef6
laitran11 <laitrant11@gmail.com>
2025-07-25 12:15:49 +0700

    Update CI workflow to run tests

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -12,12 +12,12 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'v+u=xnf8nlub*ptgeqll^ukp7w&)ykp%h6*!qnor7ik+yy636_'
+# SECRET_KEY = 'v+u=xnf8nlub*ptgeqll^ukp7w&)ykp%h6*!qnor7ik+yy636_'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
