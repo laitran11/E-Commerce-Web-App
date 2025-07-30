700c4ffe30cba95f41d8cc02a2eb20abdefa90f0
laitran11 <laitrant11@gmail.com>
2025-07-30 23:29:39 +0700

    Update CI workflow to run tests

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -101,7 +101,7 @@ DATABASES = {
         'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
         'HOST': config('DB_HOST', default='localhost'),
-        'PORT': config('DB_PORT'),
+        'PORT': config('DB_PORT', default='5432'),
     }
 }
 
